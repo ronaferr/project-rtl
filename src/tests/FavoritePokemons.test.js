f0de9a4b52aff7006fce2c281954277275dc774b
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-05-12 08:50:41 -0300

    Teste aplicado para verificação de componente "Favorite Pokemons"

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('Verifique "No favorite pokemon found", se não tiver favoritos.', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const MESSAGE_NOTFOUND = screen.getByText('No favorite pokemon found');
+  expect(MESSAGE_NOTFOUND).toBeInTheDocument();
+});
